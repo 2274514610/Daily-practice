e5908178f7f207af23c44dc29c99b6a94eef413e
lijunhui <2274514610@qq.com>
2019-12-29 20:36:57 +0800

    初识openLayers

---
+++ tianditu_map/map/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <Map></Map>
+  </div>
+</template>
+
+<script>
+  import Map from './views/map'
+  export default {
+    components: {
+      Map
+    }
+  }
+</script>
+<style lang="scss">
+
+
+</style>
